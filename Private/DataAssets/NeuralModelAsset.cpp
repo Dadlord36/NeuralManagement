ddb63567567fb4c3f4898ad252c0518d981fb373
Dadlord <Dadlord36@gmail.com>
2024-03-14 11:30:44 +0300

    NeuralModelAsset added.

---
+++ Private/DataAssets/NeuralModelAsset.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataAssets/NeuralModelAsset.h"
+
+#if WITH_EDITOR
+
+const FAIModelIdentity& UNeuralModelAsset::GetModelIdentity() const
+{
+	return ModelIdentity;
+}
+
+void UNeuralModelAsset::SetNamedDataMap(const TMap<FName, FRawData>& InNamedDataMap)
+{
+	NamedDataMap = InNamedDataMap;
+}
+
+void UNeuralModelAsset::SetRawDataBy(const FName EntryTag, const TArray<uint8>& RawData)
+{
+	NamedDataMap[EntryTag] = FRawData(RawData);
+	MarkPackageDirty();
+}
+
+const FRawData& UNeuralModelAsset::GetRawDataBy(const FName EntryTag)
+{
+	return NamedDataMap[EntryTag];
+}
+
+void UNeuralModelAsset::LoadFileInto(const FFilePath Path, TArray<uint8>& FileData)
+{
+	if (ensureAlwaysMsgf(!Path.FilePath.IsEmpty(), TEXT("Path is empty")) == false) return;
+	if (ensureAlwaysMsgf(FPaths::FileExists(Path.FilePath), TEXT("There is no file at the given path: %s"), *Path.FilePath) == false)
+	{
+		return;
+	}
+	FileData.Empty();
+	ensureAlwaysMsgf(FFileHelper::LoadFileToArray(FileData, *Path.FilePath), TEXT("Neural model data was not loaded."));
+}
+#endif
---
+++ Public/DataAssets/NeuralModelAsset.h
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "Structs/AIModelIdentity.h"
+
+#include "NeuralModelAsset.generated.h"
+
+
+USTRUCT()
+struct NEURALMANAGEMENT_API FRawData
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<uint8> Data;
+};
+
+
+UCLASS()
+class NEURALMANAGEMENT_API UNeuralModelAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	const FAIModelIdentity& GetModelIdentity() const;
+	const FRawData& GetRawDataBy(const FName EntryTag);
+	void SetNamedDataMap(const TMap<FName, FRawData>& InNamedDataMap);
+	void SetRawDataBy(const FName EntryTag, const TArray<uint8>& RawData);
+
+private:
+#if WITH_EDITOR
+	static void LoadFileInto(const FFilePath Path, TArray<uint8>& FileData);
+#endif
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	FAIModelIdentity ModelIdentity;
+	UPROPERTY()
+	TMap<FName, FRawData> NamedDataMap;
+};
